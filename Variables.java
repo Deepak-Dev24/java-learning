0c3bbe6c6aeaf919934a0ac9e41c803da58d3fc5
deepak-dev24 <dk6061051@gmail.com>
2025-06-09 12:02:19 +0530

    variable in java

---
+++ Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Integer variable
+        int n1 = 35;
+        int n2= 40;
+        int sum = n1+n2;
+        System.out.println("the sum of two number is "  + sum);
+    }
+}
