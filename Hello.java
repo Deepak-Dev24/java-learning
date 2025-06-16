c81e3a597072af4463cc740daa51255089acbc09
deepak kumar <dk6061051@gmail.com>
2025-06-16 23:31:19 +0530

    Add Java data types practice file

--- Hello.java
+++ Hello.java
@@ -1,6 +1,33 @@
 public class Hello {
-
     public static void main(String[] args) {
-        System.out.println("hello World");
+        // Integer types
+        byte a = 10;
+        short b = 2000;
+        int c = 50000;
+        long d = 1000000000L;
+
+        // Floating-point types
+        float e = 5.75f;
+        double f = 19.99;
+
+        // Character
+        char g = 'D';
+
+        // Boolean
+        boolean h = true;
+
+        // String (non-primitive)
+        String name = "Deepak";
+
+        // Output all values
+        System.out.println("byte: " + a);
+        System.out.println("short: " + b);
+        System.out.println("int: " + c);
+        System.out.println("long: " + d);
+        System.out.println("float: " + e);
+        System.out.println("double: " + f);
+        System.out.println("char: " + g);
+        System.out.println("boolean: " + h);
+        System.out.println("String: " + name);
     }
 }
