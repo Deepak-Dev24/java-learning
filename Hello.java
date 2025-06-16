971638a6b180b14ae75798f1c241bc4438d33aad
deepak kumar <dk6061051@gmail.com>
2025-06-16 23:56:48 +0530

    how to print variable

--- Hello.java
+++ Hello.java
@@ -1,33 +1,10 @@
 public class Hello {
     public static void main(String[] args) {
-        // Integer types
-        byte a = 10;
-        short b = 2000;
-        int c = 50000;
-        long d = 1000000000L;
-
-        // Floating-point types
-        float e = 5.75f;
-        double f = 19.99;
-
-        // Character
-        char g = 'D';
-
-        // Boolean
-        boolean h = true;
-
-        // String (non-primitive)
-        String name = "Deepak";
-
-        // Output all values
-        System.out.println("byte: " + a);
-        System.out.println("short: " + b);
-        System.out.println("int: " + c);
-        System.out.println("long: " + d);
-        System.out.println("float: " + e);
-        System.out.println("double: " + f);
-        System.out.println("char: " + g);
-        System.out.println("boolean: " + h);
-        System.out.println("String: " + name);
+            // write code here
+            int age = 25;
+            double height = 5.9;
+            String name= "Alex";
+            System.out.println("Name: " + name + ", Age: " + age + ", Height: " + height);
+       
     }
 }
