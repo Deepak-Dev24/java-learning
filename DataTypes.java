2ad99608395ccf7c267b0eabf69bb5392f718e03
deepak-dev24 <dk6061051@gmail.com>
2025-06-27 11:00:16 +0530

    data types in java

---
+++ DataTypes.java
@@ -0,0 +1,32 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        // Primitive Data Types
+
+        int age = 25;
+        float pi = 3.14f;
+        double salary = 45000.75;
+        char grade = 'A';
+        boolean isJavaFun = true;
+        byte smallNumber = 100;
+        short mediumNumber = 1000;
+        long bigNumber = 7800000000L;
+
+        System.out.println("Age: " + age);
+        System.out.println("PI: " + pi);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Java Fun? " + isJavaFun);
+        System.out.println("Byte Value: " + smallNumber);
+        System.out.println("Short Value: " + mediumNumber);
+        System.out.println("Long Value: " + bigNumber);
+
+
+       // Non-Primitive Data Types
+
+        String name = "Deepak";
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        System.out.println("Name: " + name);
+        System.out.println("First number: " + numbers[0]);
+}}
